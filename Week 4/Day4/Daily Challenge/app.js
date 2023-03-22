f610a5d801fbce7a732adc953e19e30ee2f46c3e
esthercherem <esthercherem18@gmail.com>
2023-03-21 19:21:56 -0600

    7th commit

--- Week 4/Day4/Daily Challenge/app.js
+++ Week 4/Day4/Daily Challenge/app.js
@@ -13,19 +13,52 @@ console.log(word)
 
 // Check out the Hints and Requirements below.
 
-const lenghtOfTheLongestWord = getLenghtOfTheLongestWord();
+let exercise = () =>{
+
+    let frase = prompt('into a phrase and separate by spaces')
+    let matriz = frase.split(' ');
+    let flags = 0;
+    for(x of matriz){
+        if(flags < x.length) {
+            flags = x.length;
+        }
+    }
+    
+    
+    let flags2 = 0;
+    let line = '';
+    for(i=0;i<flags;i++){
+        let x=0;
+        if (i === 0){
+            while(x < flags+4){
+                x++
+                line += '*'
+            }
+        }
+    }
+    console.log(line);
+    
+        if(flags2 === 0){
+            flags2++
+            let x = 0
+            let line2 = '';
+            while(x < matriz.length){
+                let a =(flags+2) - matriz[x].length
+                matriz[x] = ('* ' + matriz[x]);
+                                               
+                for(y= matriz[x].length; y<= flags+2;y++) {
+                    matriz[x] += ' '
+                }
+                matriz[x] += '*'
+                console.log(matriz[x]) 
+                x++
+                line2 += '*'
+            }
+        }
+        console.log(line);
+    
+    }
+    
+    exercise();
 
 
-function getLenghtOfTheLongestWord () {
-
-    let lenghtOfTheLongestWord = 0;
-for (const word of words) {
-     const wordLenght = word.lenght;
-     if (wordLenght > lenghtOfTheLongestWord) {
-        lenghtOfTheLongestWord = wordLenght;
-     }
-}
-   
-
-
-}
